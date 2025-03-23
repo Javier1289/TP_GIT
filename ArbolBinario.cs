ae6b6c8ef8545a14ac8a29a7cf327a4578b42ae9
Javier Rojas <j.rojas1289@gmail.com>
2025-03-23 02:48:49 -0300

    Ejercicios de Clase | Ejercicios de TP: puntos 4 y 5

---
+++ ArbolBinario.cs
@@ -0,0 +1,176 @@
+﻿using System;
+
+namespace tp_01
+{
+	public class ArbolBinario<T>
+	{
+		
+		private T dato;
+		private ArbolBinario<T> hijoIzquierdo;
+		private ArbolBinario<T> hijoDerecho;
+	
+		
+		public ArbolBinario(T dato) {
+			this.dato = dato;
+		}
+	
+		public T getDatoRaiz() {
+			return this.dato;
+		}
+	
+		public ArbolBinario<T> getHijoIzquierdo() {
+			return this.hijoIzquierdo;
+		}
+	
+		public ArbolBinario<T> getHijoDerecho() {
+			return this.hijoDerecho;
+		}
+	
+		public void agregarHijoIzquierdo(ArbolBinario<T> hijo) {
+			this.hijoIzquierdo=hijo;
+		}
+	
+		public void agregarHijoDerecho(ArbolBinario<T> hijo) {
+			this.hijoDerecho=hijo;
+		}
+	
+		public void eliminarHijoIzquierdo() {
+			this.hijoIzquierdo=null;
+		}
+	
+		public void eliminarHijoDerecho() {
+			this.hijoDerecho=null;
+		}
+	
+		public bool esHoja() {
+			return this.hijoIzquierdo==null && this.hijoDerecho==null;
+		}
+		
+		public void inorden() {
+			
+			if( this.getHijoIzquierdo() != null ){
+				this.getHijoIzquierdo().inorden();
+			}
+			
+			Console.WriteLine( this.getDatoRaiz() );
+			
+			if( this.getHijoDerecho() != null ){
+				this.getHijoDerecho().inorden();
+			}
+			
+		}
+		
+		public void preorden() {
+	
+			Console.WriteLine( this.getDatoRaiz() );
+			
+			if( this.getHijoIzquierdo() != null ){
+				this.getHijoIzquierdo().preorden();
+			}
+			
+			
+			if( this.getHijoDerecho() != null ){
+				this.getHijoDerecho().preorden();
+			}
+			
+		}
+		
+		public void postorden() {
+			
+			if( this.getHijoIzquierdo() != null ){			
+				this.getHijoIzquierdo().postorden();				
+			}			
+			
+			if( this.getHijoDerecho() != null ){
+				this.getHijoDerecho().postorden();
+			}
+			
+			Console.WriteLine( this.getDatoRaiz() );
+		}
+		
+		public void recorridoPorNiveles() {
+			
+			Cola<ArbolBinario<T>> cola = new Cola<ArbolBinario<T>>();
+			Cola<int> niveles = new Cola<int>();			
+
+			cola.encolar( this );
+			niveles.encolar( 1 );
+			
+			while( !cola.esVacia() ){
+				
+				ArbolBinario<T> aux = cola.desencolar();
+				int nivel = niveles.desencolar();
+				
+				Console.Write(nivel);
+				Console.Write( " ===> ");
+				
+				Console.WriteLine( aux.getDatoRaiz());
+				
+				if( aux.getHijoIzquierdo() != null ){
+					cola.encolar( aux.getHijoIzquierdo() );
+					niveles.encolar( nivel + 1);
+				}
+				
+				if( aux.getHijoDerecho() != null ){
+					cola.encolar( aux.getHijoDerecho() );
+					niveles.encolar( nivel + 1);
+				}
+
+			}
+			
+		}
+	
+		public int contarHojas() {
+			int hojas = 0;
+			if( this.esHoja() ){
+				return 1;	   	
+			}
+			
+			if( this.getHijoIzquierdo() != null ){
+				hojas += this.getHijoIzquierdo().contarHojas();
+			}
+			
+			if( this.getHijoDerecho() != null ){
+				hojas += this.getHijoDerecho().contarHojas();
+			}
+						
+			return hojas;
+		}
+		
+		public void recorridoEntreNiveles(int n,int m) {
+			
+						
+			Cola<ArbolBinario<T>> cola = new Cola<ArbolBinario<T>>();
+			Cola<int> niveles = new Cola<int>();
+
+			cola.encolar( this );
+			niveles.encolar( 1 );
+			
+			while( !cola.esVacia() ){
+				
+				ArbolBinario<T> aux = cola.desencolar();
+				int nivel = niveles.desencolar();
+			
+				if( nivel >= n && nivel <= m ){
+					Console.Write(nivel);
+					Console.Write( " ===> ");				
+					Console.WriteLine( aux.getDatoRaiz());					
+				}
+
+				
+				if( aux.getHijoIzquierdo() != null ){
+					cola.encolar( aux.getHijoIzquierdo() );
+					niveles.encolar( nivel + 1);
+				}
+				
+				if( aux.getHijoDerecho() != null ){
+					cola.encolar( aux.getHijoDerecho() );
+					niveles.encolar( nivel + 1);
+				}
+
+			}
+			
+		}
+	
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: PC-Developer
+ * Date: 23/3/2025
+ * Time: 00:24
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace tp_01
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+			ArbolBinario<int> arbol1 = new ArbolBinario<int>(1);
+			ArbolBinario<int> arbol2 = new ArbolBinario<int>(2);
+			ArbolBinario<int> arbol3 = new ArbolBinario<int>(3);
+			ArbolBinario<int> arbol4 = new ArbolBinario<int>(4);
+			ArbolBinario<int> arbol5 = new ArbolBinario<int>(5);
+			ArbolBinario<int> arbol6 = new ArbolBinario<int>(6);
+			ArbolBinario<int> arbol7 = new ArbolBinario<int>(7);
+			ArbolBinario<int> arbol8 = new ArbolBinario<int>(8);
+			ArbolBinario<int> arbol9 = new ArbolBinario<int>(9);
+			
+
+			arbol1.agregarHijoIzquierdo(arbol2);
+			arbol1.agregarHijoDerecho(arbol3);
+			arbol2.agregarHijoIzquierdo(arbol4);
+			arbol2.agregarHijoDerecho(arbol5);
+			arbol3.agregarHijoIzquierdo(arbol6);
+			arbol3.agregarHijoDerecho(arbol7);
+			arbol4.agregarHijoIzquierdo(arbol8);
+			arbol4.agregarHijoDerecho(arbol9);
+			
+
+			Console.WriteLine("***Inorden****");
+
+			arbol1.inorden();
+			
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			
+			Console.WriteLine("***Preorden****");
+			arbol1.preorden();
+			
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			
+			Console.WriteLine("***Postorden****");			
+			arbol1.postorden();
+			
+	
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			
+			Console.WriteLine("***Por Niveles****");						
+			arbol1.recorridoPorNiveles();
+			
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			
+			Console.WriteLine("***Contar Hojas****");						
+			Console.WriteLine( arbol1.contarHojas());
+			
+
+			Console.WriteLine("");
+			Console.WriteLine("");
+			Console.WriteLine("");
+			
+			Console.WriteLine("***Entre Niveles****");						
+			arbol1.recorridoEntreNiveles( 4, 4 );
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
